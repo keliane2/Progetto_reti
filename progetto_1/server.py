6828a13d6a8d8b9299049d881eb5816bc4828e0a
keliane2 <kelianetchoumkeu@gmail.com>
2024-07-05 18:48:12 +0200

    Modified server.py

--- progetto_1/server.py
+++ progetto_1/server.py
@@ -2,58 +2,68 @@
 # -*- coding: utf-8 -*-
 #server
 import socket
-import threading
+from threading import Thread
 
 # IP address del server ricevente
-SERVER_HOST = "0.0.0.0"
+SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 5001
-# riceve 4096 bytes ogni trance
+# riceve 4096 bytes
 BUFFER_SIZE = 1024
 #numero di connessioni abilitate
-NUM = 100
+NUM = 50
 
-# Lista dei clienti connessi
+# Lista per tener traccia dei client connessi
 clients = []
 
-# Funzione per ricevere i messaggi ricevuti dai clients
-def handle_client(client_socket):
-    while True:
-        try:
-            #riceve il messaggio dal cliente
-            message = client_socket.recv(BUFFER_SIZE).decode('utf-8','ignore')
-            if message:
-                print(f"Received message: {message}")
-                #condivide il messaggio con tutti gli altri client
-                broadcast(message, client_socket)
-            else:
-                remove(client_socket)
-        except:
-            continue
-
-# Funzione per inviare messaggi a tutti i clienti connessi
-def broadcast(message, connection):
+def broadcast(message, client_socket):
+    # Invia il messaggio agli altri client
     for client in clients:
-        if client != connection:
+        if client != client_socket:
             try:
                 client.send(message.encode('utf-8'))
             except:
+                #Rimuove il client nel caso l'invio non andasse
+                #a buon fine
                 remove(client)
 
-# Funzione per rimuovere un client dalla lista dei clienti connessi
+# La funzione gestisce la connessione di un singolo client.
+def manage_client(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(BUFFER_SIZE).decode('utf-8')
+            if message:
+                print(f"Received message: {message}")
+                broadcast(message, client_socket)
+            else:
+                remove(client_socket)
+                break
+        except:
+            continue
+
+def receive_connections():
+    while True:
+        client_socket, client_address = server.accept()
+        print(f"Connection from {client_address} has been established.")
+        #aggiunge il nuovo client che si connette alla lista dei client connessi
+        clients.append(client_socket)
+        client_socket.send(bytes("Hello, connection to the server ok!","utf-8"))
+        #diamo inizio all'attività del Thread - uno per ciascun client
+        Thread(target=manage_client, args=(client_socket,)).start()
+
+# Funzione che rimuove un client dalla lista dei 
+# client connessi nel caso non fosse più connesso
 def remove(client):
     if client in clients:
         clients.remove(client)
 
-# main principale per il server
 if __name__ == "__main__":
+    # Creazione del socket per il server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((SERVER_HOST, SERVER_PORT))
-    server.listen(NUM)
-    print("Server started. Waiting for connections...")
+    server.listen(NUM)    
+    print("Server waiting for connections...")
+    ACCEPT_THREAD = Thread(target=receive_connections()).start()
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    server.close()
 
-    while True:
-        client_socket, addr = server.accept()
-        clients.append(client_socket)
-        print(f"Connection from {addr} has been established.")
-
-        threading.Thread(target=handle_client, args=(client_socket,)).start()
